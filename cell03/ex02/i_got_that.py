eff09a896d3f12062feb5df6aaed9364c403f68d
psrapril <ppphan23@gmail.com>
2026-09-22 23:59:48 +0700

    while stop

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+message = input("What you gotta say? : ")
+while True:
+    if message == "STOP":
+        break
+    else: 
+        message = input("I got that! Anything else? : ")
