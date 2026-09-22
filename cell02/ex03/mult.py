d083633b10759fb1324d3e1140601a0f664abb4d
psrapril <ppphan23@gmail.com>
2026-09-22 21:53:17 +0700

    multi

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-a = int(input("Enter the first number: \n"))
-b = int(input("Enter the second number: \n"))
+a = int(input("Enter the first number:\n"))
+b = int(input("Enter the second number:\n"))
 result =  a * b
-print(f"{a} * {b} = {result}")
+print(f"{a} x {b} = {result}")
 if result < 0:
     print("The result is negative.")
 elif result > 0:
