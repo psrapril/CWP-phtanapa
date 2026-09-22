48e81294778659c7e70d4756b94920387eb0a4d6
psrapril <ppphan23@gmail.com>
2026-09-22 21:46:57 +0700

    multi

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+a = int(input("Enter the first number: \n"))
+b = int(input("Enter the second number: \n"))
+result =  a * b
+print(f"{a} * {b} = {result}")
+if result < 0:
+    print("The result is negative.")
+elif result > 0:
+    print("The result is positive.")
+else:
+    print("The result is positive and negative.")
