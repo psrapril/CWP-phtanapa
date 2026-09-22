31492f9c74aae3d74620559d5cf811edfdd07c7f
psrapril <ppphan23@gmail.com>
2026-09-22 19:12:39 +0700

    if-else number

---
+++ cell02/ex01/isnag.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+number = int(input(""))
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("This number is positive.")
+else:
+    print("This number is equal to zero.")
